f9726cb04aecf5af31af68d67cc08eec42963072
SeungBeomLim <143402775+SeungBeomLim@users.noreply.github.com>
2025-07-22 10:42:31 +0900

    Update license

--- lib/provider/reservation_provider.dart
+++ lib/provider/reservation_provider.dart
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */ 
 
 import 'package:flutter/material.dart';
 import 'package:camticket/model/reservation_list_model.dart';
