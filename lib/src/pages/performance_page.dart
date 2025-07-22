52adfb7cdea64c3a71303e332b0a087053b7b23b
SeungBeomLim <143402775+SeungBeomLim@users.noreply.github.com>
2025-07-22 10:33:34 +0900

    Update license

--- lib/src/pages/performance_page.dart
+++ lib/src/pages/performance_page.dart
@@ -1,4 +1,22 @@
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  api는 연결했지만 각 데이터별로 값을 표시하는 건 아직 작업하지 않았습니다.
  이부분만 작업하면 될 것 같습니다.
  */
